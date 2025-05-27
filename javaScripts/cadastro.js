35b542e17b334a1390df77cf2fc1fdec7eebc250
Alexandre Douglas <alexandredouglas16@gmail.com>
2025-05-27 18:28:34 -0300

    nada dms

--- javaScripts/cadastro.js
+++ javaScripts/cadastro.js
@@ -1,17 +1,34 @@
 function pegar_dados() {
-    nome = document.getElementById("nome").value
-    email = document.getElementById("email").value
-    senha = document.getElementById("senha").value
-    classe = document.getElementsByTagName("radio")
-    icon = classe[0]
+    let nome = document.getElementById("nome").value
+    let email = document.getElementById("email").value
+    let senha = document.getElementById("senha").value
+    let classe = document.querySelector('input[name="classe"]:checked').value;
 
     dados = {
         "nome": nome,
         "email": email,
         "senha": senha,
-        "icon": icon
+        "icon": classe
     }
 
     window.localStorage.setItem("dados_usuario", JSON.stringify(dados))
-    console.log("peguei!")
+}
+
+function logar(user,senha) {
+    const dados_cadastro = window.localStorage.getItem("dados_usuario")
+
+    if (!dados_cadastro) {
+        alert("Nenhum usuário cadastrado!");
+        location.reload();
+        return;
+    }
+
+    if ((user == dados_cadastro['nome'] || user == dados_cadastro['email']) && (senha == dados_cadastro['senha']) ) {
+        alert("Logado com Sucesso!")
+
+        window.location.href = "index.html";
+    } else {
+        alert("Usuário ou senha incorretos.");
+        location.reload();
+    }
 }
